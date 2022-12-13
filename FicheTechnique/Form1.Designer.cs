3be9d30289e6afc484638375da10caa2c14cc065
memer <memer@DESKTOP-6O4RA11>
2022-12-12 22:22:45 -0500

    Ajoutez des fichiers projet.

---
+++ FicheTechnique/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace FicheTechnique
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCPU = new System.Windows.Forms.Label();
+            this.CPUNAME = new System.Windows.Forms.Label();
+            this.lbDiskName = new System.Windows.Forms.Label();
+            this.DISKNAME = new System.Windows.Forms.Label();
+            this.SizeName = new System.Windows.Forms.Label();
+            this.lbDiskSize = new System.Windows.Forms.Label();
+            this.lbMemorySize = new System.Windows.Forms.Label();
+            this.MemorySize = new System.Windows.Forms.Label();
+            this.lbDiskName1 = new System.Windows.Forms.Label();
+            this.lbDiskSize1 = new System.Windows.Forms.Label();
+            this.lbGPUName = new System.Windows.Forms.Label();
+            this.GPUNAME = new System.Windows.Forms.Label();
+            this.lbGPUName1 = new System.Windows.Forms.Label();
+            this.lbRes = new System.Windows.Forms.Label();
+            this.CURRES = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbCPU
+            // 
+            this.lbCPU.AutoSize = true;
+            this.lbCPU.Location = new System.Drawing.Point(363, 103);
+            this.lbCPU.Name = "lbCPU";
+            this.lbCPU.Size = new System.Drawing.Size(29, 13);
+            this.lbCPU.TabIndex = 0;
+            this.lbCPU.Text = "CPU";
+            // 
+            // CPUNAME
+            // 
+            this.CPUNAME.AutoSize = true;
+            this.CPUNAME.Font = new System.Drawing.Font("Roland", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPUNAME.Location = new System.Drawing.Point(273, 94);
+            this.CPUNAME.Name = "CPUNAME";
+            this.CPUNAME.Size = new System.Drawing.Size(62, 28);
+            this.CPUNAME.TabIndex = 1;
+            this.CPUNAME.Text = "CPU";
+            // 
+            // lbDiskName
+            // 
+            this.lbDiskName.AutoSize = true;
+            this.lbDiskName.Location = new System.Drawing.Point(363, 152);
+            this.lbDiskName.Name = "lbDiskName";
+            this.lbDiskName.Size = new System.Drawing.Size(56, 13);
+            this.lbDiskName.TabIndex = 2;
+            this.lbDiskName.Text = "DiskName";
+            // 
+            // DISKNAME
+            // 
+            this.DISKNAME.AutoSize = true;
+            this.DISKNAME.Font = new System.Drawing.Font("Roland", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DISKNAME.Location = new System.Drawing.Point(193, 143);
+            this.DISKNAME.Name = "DISKNAME";
+            this.DISKNAME.Size = new System.Drawing.Size(142, 28);
+            this.DISKNAME.TabIndex = 3;
+            this.DISKNAME.Text = "Disk Drive";
+            // 
+            // SizeName
+            // 
+            this.SizeName.AutoSize = true;
+            this.SizeName.Font = new System.Drawing.Font("Roland", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SizeName.Location = new System.Drawing.Point(279, 180);
+            this.SizeName.Name = "SizeName";
+            this.SizeName.Size = new System.Drawing.Size(56, 24);
+            this.SizeName.TabIndex = 4;
+            this.SizeName.Text = "Size";
+            // 
+            // lbDiskSize
+            // 
+            this.lbDiskSize.AutoSize = true;
+            this.lbDiskSize.Location = new System.Drawing.Point(363, 187);
+            this.lbDiskSize.Name = "lbDiskSize";
+            this.lbDiskSize.Size = new System.Drawing.Size(48, 13);
+            this.lbDiskSize.TabIndex = 5;
+            this.lbDiskSize.Text = "DiskSize";
+            // 
+            // lbMemorySize
+            // 
+            this.lbMemorySize.AutoSize = true;
+            this.lbMemorySize.Location = new System.Drawing.Point(363, 219);
+            this.lbMemorySize.Name = "lbMemorySize";
+            this.lbMemorySize.Size = new System.Drawing.Size(67, 13);
+            this.lbMemorySize.TabIndex = 6;
+            this.lbMemorySize.Text = "Memory Size";
+            // 
+            // MemorySize
+            // 
+            this.MemorySize.AutoSize = true;
+            this.MemorySize.Font = new System.Drawing.Font("Roland", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemorySize.Location = new System.Drawing.Point(146, 210);
+            this.MemorySize.Name = "MemorySize";
+            this.MemorySize.Size = new System.Drawing.Size(189, 28);
+            this.MemorySize.TabIndex = 7;
+            this.MemorySize.Text = "Total Memory";
+            // 
+            // lbDiskName1
+            // 
+            this.lbDiskName1.AutoSize = true;
+            this.lbDiskName1.Location = new System.Drawing.Point(551, 152);
+            this.lbDiskName1.Name = "lbDiskName1";
+            this.lbDiskName1.Size = new System.Drawing.Size(68, 13);
+            this.lbDiskName1.TabIndex = 8;
+            this.lbDiskName1.Text = "Disk Name 2";
+            // 
+            // lbDiskSize1
+            // 
+            this.lbDiskSize1.AutoSize = true;
+            this.lbDiskSize1.Location = new System.Drawing.Point(551, 187);
+            this.lbDiskSize1.Name = "lbDiskSize1";
+            this.lbDiskSize1.Size = new System.Drawing.Size(60, 13);
+            this.lbDiskSize1.TabIndex = 9;
+            this.lbDiskSize1.Text = "Disk Size 2";
+            // 
+            // lbGPUName
+            // 
+            this.lbGPUName.AutoSize = true;
+            this.lbGPUName.Location = new System.Drawing.Point(363, 265);
+            this.lbGPUName.Name = "lbGPUName";
+            this.lbGPUName.Size = new System.Drawing.Size(61, 13);
+            this.lbGPUName.TabIndex = 10;
+            this.lbGPUName.Text = "GPU Name";
+            // 
+            // GPUNAME
+            // 
+            this.GPUNAME.AutoSize = true;
+            this.GPUNAME.Font = new System.Drawing.Font("Roland", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GPUNAME.Location = new System.Drawing.Point(155, 256);
+            this.GPUNAME.Name = "GPUNAME";
+            this.GPUNAME.Size = new System.Drawing.Size(180, 28);
+            this.GPUNAME.TabIndex = 11;
+            this.GPUNAME.Text = "Graphic Card";
+            // 
+            // lbGPUName1
+            // 
+            this.lbGPUName1.AutoSize = true;
+            this.lbGPUName1.Location = new System.Drawing.Point(554, 264);
+            this.lbGPUName1.Name = "lbGPUName1";
+            this.lbGPUName1.Size = new System.Drawing.Size(70, 13);
+            this.lbGPUName1.TabIndex = 12;
+            this.lbGPUName1.Text = "GPU Name 2";
+            // 
+            // lbRes
+            // 
+            this.lbRes.AutoSize = true;
+            this.lbRes.Location = new System.Drawing.Point(363, 313);
+            this.lbRes.Name = "lbRes";
+            this.lbRes.Size = new System.Drawing.Size(94, 13);
+            this.lbRes.TabIndex = 13;
+            this.lbRes.Text = "Current Resolution";
+            // 
+            // CURRES
+            // 
+            this.CURRES.AutoSize = true;
+            this.CURRES.Font = new System.Drawing.Font("Roland", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CURRES.Location = new System.Drawing.Point(80, 304);
+            this.CURRES.Name = "CURRES";
+            this.CURRES.Size = new System.Drawing.Size(255, 28);
+            this.CURRES.TabIndex = 14;
+            this.CURRES.Text = "Current Resolution";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 572);
+            this.Controls.Add(this.CURRES);
+            this.Controls.Add(this.lbRes);
+            this.Controls.Add(this.lbGPUName1);
+            this.Controls.Add(this.GPUNAME);
+            this.Controls.Add(this.lbGPUName);
+            this.Controls.Add(this.lbDiskSize1);
+            this.Controls.Add(this.lbDiskName1);
+            this.Controls.Add(this.MemorySize);
+            this.Controls.Add(this.lbMemorySize);
+            this.Controls.Add(this.lbDiskSize);
+            this.Controls.Add(this.SizeName);
+            this.Controls.Add(this.DISKNAME);
+            this.Controls.Add(this.lbDiskName);
+            this.Controls.Add(this.CPUNAME);
+            this.Controls.Add(this.lbCPU);
+            this.Name = "Form1";
+            this.Text = "Fiche Technique";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCPU;
+        private System.Windows.Forms.Label CPUNAME;
+        private System.Windows.Forms.Label lbDiskName;
+        private System.Windows.Forms.Label DISKNAME;
+        private System.Windows.Forms.Label SizeName;
+        private System.Windows.Forms.Label lbDiskSize;
+        private System.Windows.Forms.Label MemorySize;
+        public System.Windows.Forms.Label lbMemorySize;
+        private System.Windows.Forms.Label lbDiskName1;
+        private System.Windows.Forms.Label lbDiskSize1;
+        private System.Windows.Forms.Label lbGPUName;
+        private System.Windows.Forms.Label GPUNAME;
+        private System.Windows.Forms.Label lbGPUName1;
+        private System.Windows.Forms.Label lbRes;
+        private System.Windows.Forms.Label CURRES;
+    }
+}
+
---
+++ FicheTechnique/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using Microsoft.VisualBasic.Devices;
+using System.IO;
+
+namespace FicheTechnique
+{
+    public partial class Form1 : Form
+    {
+        public static ulong Ram;
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getCpuName()
+        {
+            ManagementObjectSearcher mos =
+           new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                lbCPU.Text = (string)mo["Name"];
+
+            }
+
+        }
+        public void getAvailableRAM()
+        {
+            ComputerInfo CI = new ComputerInfo();
+            ulong mem = ulong.Parse(CI.TotalPhysicalMemory.ToString());
+            lbMemorySize.Text = (mem / (1024 * 1024) + " MB").ToString();
+        }
+        public void getStorage()
+        {
+             int i = 0;
+             ulong[] storage = new ulong[10];
+             string[] DiskNames = new string[10];
+        ManagementObjectSearcher mos1 =
+           new ManagementObjectSearcher("root\\CIMV2", @"SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject mo in mos1.Get())
+            {
+                DiskNames[i] = (string)mo["Model"];
+                storage[i] = ulong.Parse(mo["Size"].ToString());
+                
+                i++;
+            }
+            lbDiskName.Text = DiskNames[0];
+            lbDiskName1.Text = DiskNames[1];
+            lbDiskSize.Text = (storage[0] / (1000 * 1000 * 1000) + " GB").ToString();
+            lbDiskSize1.Text = (storage[1] / (1000 * 1000 * 1000) + " GB").ToString();
+        }
+        public void getGpuName()
+        {
+            int i = 0;
+            string[] gpu = new string[10];
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            foreach(ManagementObject mo in mos.Get())
+            {
+              gpu[i] = mo["Name"].ToString();
+              i++;
+            }
+            lbGPUName.Text = gpu[0];
+            lbGPUName1.Text = gpu[1];
+        }
+        public void getCurrentResolution()
+        {
+            string screenWidth = Screen.PrimaryScreen.Bounds.Width.ToString();
+            string screenHeight = Screen.PrimaryScreen.Bounds.Height.ToString();
+            lbRes.Text = ("Resolution: " + screenWidth + " x" + screenHeight);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            getCpuName();
+            getStorage();
+            getAvailableRAM();
+            getGpuName();
+            getCurrentResolution();
+        }
+    }
+}
